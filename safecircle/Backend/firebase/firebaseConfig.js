e15a43a2eb69a338eee3774794a60c721c676f82
Hannah Huynh <hannahnh@uci.edu>
2024-05-25 03:33:23 -0700

    correct firebaseConfig

--- safecircle/Backend/firebase/firebaseConfig.js
+++ safecircle/Backend/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_WEB_APP_ID, FIREBASE_MEASUREMENT_ID } from "@env";
+import { FIREBASE_API_KEY, FIREBASE_AUTH_KEY, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_WEB_APP_ID, FIREBASE_MEASUREMENT_ID } from "@env";
 
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
+  authDomain: FIREBASE_AUTH_KEY,
   projectId: FIREBASE_PROJECT_ID,
   storageBucket: FIREBASE_STORAGE_BUCKET,
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 // Validate that all necessary environment variables are loaded
-if (!firebaseConfig.apiKey || !firebaseConfig.authDomain || !firebaseConfig.projectId || !firebaseConfig.storageBucket || !firebaseConfig.messagingSenderId || !firebaseConfig.appId || !firebaseConfig.measurementId) {
+if (!firebaseConfig.apiKey && !firebaseConfig.authDomain && !firebaseConfig.projectId && !firebaseConfig.storageBucket && !firebaseConfig.messagingSenderId && !firebaseConfig.appId && !firebaseConfig.measurementId) {
   throw new Error("Missing Firebase configuration values. Please check your .env file.");
 }
 
